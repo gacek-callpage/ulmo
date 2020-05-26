d9455ac0d8f39b5f957d0e89c60dc12e62ce80ab
Gacek <szymon.gackowskii@gmail.com>
2020-05-26 15:02:16 +0200

    moved from db to api

--- measurements.py
+++ measurements.py
@@ -2,29 +2,20 @@
 
 import sys
 import Adafruit_DHT
-import mysql.connector
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
-def save_to_db(db, values):
-    
-    mycursor = db.cursor()
+def save_to_api(temp, hum):
+    hed = {'Authorization': 'Bearer ' + os.getenv('API_TOKEN')}
+    data = {'temp': temp, 'humidity': hum}
 
-    sql = "INSERT INTO measurements (temp, humidity) VALUES (%s, %s)"
-    mycursor.execute(sql, values)
-
-    db.commit()
+    response = requests.post( os.getenv('API_HOST')+'/api/measurements', json=data, headers=hed)
+    print(response)
 
 # ------------------------------------------------------------------------
-
-mydb = mysql.connector.connect(
-  host=os.getenv('DB_SERVER'),
-  user=os.getenv('DB_USER'),
-  passwd=os.getenv('DB_PASSWORD'),
-  database=os.getenv('DB_DATABASE')
-)
 
 sensor = Adafruit_DHT.DHT11
 pin = os.getenv('PIN_DHT11')
@@ -35,8 +26,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-    val = (temperature, humidity)
-    save_to_db(mydb, val);
+    save_to_api(temperature, humidity)
 else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
